bdd00768e570c043fbcf047c96d795de8d10d064
Do Huynh <huynhdoo@gmail.com>
2025-10-03 10:38:51 +0000

    feat: add proxy auth

--- lib/discussion.py
+++ lib/discussion.py
@@ -3,6 +3,8 @@ import json
 import os
 
 RAG_API_ENPOINT = os.getenv('RAG_API_ENDPOINT')
+RAG_API_KEY = os.getenv('RAG_API_KEY')
+RAG_API_SECRET = os.getenv('RAG_API_SECRET')
 
 class Message:
     order:int
@@ -12,9 +14,10 @@ class Message:
     responses:list
     final_response:str
     
-def send_rag(docs, message, model="openai:gpt-4o-mini"):
+def send_rag(docs, message, model="openai:gpt-5-nano"):
     response = requests.post(
         url=RAG_API_ENPOINT,
-        json={"docs":docs, "message":message, "model":model})
+        json={"docs":docs, "message":message, "model":model}),
+        headers={"Modal-Key": RAG_API_KEY, "Modal-Secret": RAG_API_SECRET}
     return response.json()
     
